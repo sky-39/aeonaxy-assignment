dc13f2a139255a93e6ea62b86c3923cec9428f18
Akash Tiwari <akash_t@bt.iitr.ac.in>
2024-04-09 07:06:55 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -2,15 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDb from "./db/connectToMongoDb.js";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
-app.use(express.json({limit: '25mb'}));
+app.use(express.json({ limit: "25mb" }));
 
 app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 
 app.listen(8000, () => {
   connectToMongoDb();
