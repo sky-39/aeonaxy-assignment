64e6107be6d361cbaf60c7f091403a4ed99bd51b
Akash Tiwari <akash_t@bt.iitr.ac.in>
2024-04-14 17:42:05 +0530

    final

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
-import connectToMongoDb from "./db/connectToMongoDb.js";
+import connectToMongoDb from "./db/connectToMongoDB.js";
 import path from "path";
 
 const app = express();
